b47ce7ba7f8189216abe4dfa4d27c5dff85afa80
Brian <brianmelaradev@gmail.com>
2021-05-23 23:45:30 -0400

    leastLarger7k

---
+++ leastLarger.js
@@ -0,0 +1,11 @@
+// Task
+// Given an array of numbers and an index, return the index of the least number larger than the element at the given index, or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+
+// Notes
+// Multiple correct answers may be possible. In this case, return any one of them.
+// The given index will be inside the given array.
+// The given array will, therefore, never be empty.
+function leastLarger(a, i) {
+    let target = a[i], x = a.filter(num => num > target), y = Math.min(...x);
+    return a.findIndex(num => num == y)
+}
